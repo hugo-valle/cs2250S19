0dacc49c076b0dccea76a104ef3ff8b1fd408efa
Hugo Valle <hugovalle1@weber.edu>
2019-01-22 09:03:40 -0700

    Initial commit

---
+++ mod3/chars.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  chars.c
+ *
+ *    Description:  Learn how to use characters
+ *
+ *        Version:  1.0
+ *        Created:  01/22/2019 08:53:35 AM
+ *       Revision:  none
+ *       Compiler:  gcc chars.c -o chars.out 
+ *
+ *         Author:  Hugo Valle (), hugovalle1@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int age = 20;
+    double weight = 178.3;
+    char sex = 'M';  // Use single ticks
+
+    printf("Your age is %d and you weight %lf and your sex is %c\n",
+            age, weight, sex);
+
+    return 0;
+}
+// Function Definitions
+
+
