d505def6bd71978f9a724e118b5c7d0b02a10f6e
Hugo Valle <hugovalle1@weber.edu>
2019-01-24 09:31:23 -0700

    Copy and appended strings to fullName

--- mod3/stringsFunc.c
+++ mod3/stringsFunc.c
@@ -21,15 +21,17 @@
 
 // Main Function
 int main()
-{
+{    // to autoindent, while seating in one curly type: =%
     char first[50];
     char last[50];
- char fullName[100];
+    char fullName[100];
     printf("Enter your first and last name: ");
-         scanf("%s %s", first, last);
-     printf("Hi [%s] [%s]\n", first, last);
-strcpy(fullName, first);
-printf("Your Full name is: [%s]\n", fullName);
+    scanf("%s %s", first, last);
+    printf("Hi [%s] [%s]\n", first, last);
+    strcpy(fullName, first);    // fullName = first
+    strcat(fullName, " ");      // fullName = fullName + " "
+    strcat(fullName, last);      // fullName = fullName + last
+    printf("Your Full name is: [%s]\n", fullName);
 
     return 0;
 }
