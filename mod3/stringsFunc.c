307bcbd1cfa2380dd4c6dac18db9b1304e9f8374
Hugo Valle <hugovalle1@weber.edu>
2019-01-24 09:20:58 -0700

    Initial commit

---
+++ mod3/stringsFunc.c
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  stringsFunc.c
+ *
+ *    Description:  Learn some string functions
+ *
+ *        Version:  1.0
+ *        Created:  01/24/2019 09:08:19 AM
+ *       Revision:  none
+ *       Compiler:  gcc stringsFunc.c -o stringsFunc.out 
+ *
+ *         Author:  Hugo Valle (), hugovalle1@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <string.h>         // for string manipulation operations
+
+
+// Main Function
+int main()
+{
+    char first[50];
+    char last[50];
+ char fullName[100];
+    printf("Enter your first and last name: ");
+         scanf("%s %s", first, last);
+     printf("Hi [%s] [%s]\n", first, last);
+strcpy(fullName, first);
+printf("Your Full name is: [%s]\n", fullName);
+
+    return 0;
+}
