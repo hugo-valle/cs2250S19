87b4bbe58ce9d54452b0ff9d1c4f7ae2cd92eb29
Hugo Valle <hugovalle1@weber.edu>
2019-01-24 08:58:18 -0700

    Calculated the cartesian coordinates

--- mod3/cartesian.c
+++ mod3/cartesian.c
@@ -17,19 +17,28 @@
  * =====================================================================================
  */
 #include <stdio.h>
+#include <math.h>
 
 // Constants
+const double PI = 3.14159;
 
 // Main Function
 int main()
 {
+    double radius, theta, x_pos, y_pos;
     // 1) Capture user input for radius and theta angle(degrees)
- 
+    printf("Enter your radius: ");
+    scanf("%lf", &radius);
+    printf("Enter your theta angle in degrees: ");
+    scanf("%lf", &theta);
     // 2) Convert the theta angle to radians
-    
+    theta = (theta*PI/180.0);
     // 3) Calculate your cartesian coordinates
-    
+    x_pos = radius*cos(theta);
+    y_pos = radius*sin(theta);
     // 4) Print the result
+    printf("Your Cartesian Coordinates, x = [%lf], y = [%lf]\n",
+            x_pos, y_pos);
 
     return 0;
 }
