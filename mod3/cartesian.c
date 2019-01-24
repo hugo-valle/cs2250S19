9a8c654b9e24b00d74286c8a5cc8a22a0023bcec
Hugo Valle <hugovalle1@weber.edu>
2019-01-24 08:42:37 -0700

    Initial commit

---
+++ mod3/cartesian.c
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  cartesian.c
+ *
+ *    Description:  Calculate the cartesian coordinates based on:
+ *                  radius and theta angle(degress)
+ *
+ *        Version:  1.0
+ *        Created:  01/24/2019 08:35:51 AM
+ *       Revision:  none
+ *       Compiler:  gcc cartesian.c -o cartesian.out -lm 
+ *
+ *         Author:  Hugo Valle (), hugovalle1@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Main Function
+int main()
+{
+    // 1) Capture user input for radius and theta angle(degrees)
+ 
+    // 2) Convert the theta angle to radians
+    
+    // 3) Calculate your cartesian coordinates
+    
+    // 4) Print the result
+
+    return 0;
+}
