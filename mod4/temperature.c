8b954676c099a3c34e2f08f75910589618073241
Hugo Valle <hugovalle1@weber.edu>
2019-01-29 10:16:01 -0700

    auto indent code

--- mod4/temperature.c
+++ mod4/temperature.c
@@ -85,6 +85,11 @@ int main()
 			water_state = BOIL;
 		}
 		// && -> AND 
+		// || -> OR
+		// ! -> NOT
+		// == 
+		// != 
+		// >, < , >=, <=
 		else if (in_temp > FREEZE_FAHR && in_temp < BOIL_CEL)
 		{
 			water_state = LIQUID;
