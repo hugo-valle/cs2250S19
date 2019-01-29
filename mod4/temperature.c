67524549ade253e868722b878e4a06d0c5ef1d91
Hugo Valle <hugovalle1@weber.edu>
2019-01-29 09:09:40 -0700

    Add user choice of temperature

--- mod4/temperature.c
+++ mod4/temperature.c
@@ -1,14 +1,14 @@
 /*
  * =====================================================================================
  *
- *       Filename:  float.c
+ *       Filename:  temperature.c
  *
- *    Description:  Play with float points.
+ *    Description:  Program to calculate temperature
  *
  *        Version:  1.0
  *        Created:  01/15/2019 09:26:42 AM
  *       Revision:  none
- *       Compiler:  gcc float.c -o float
+ *       Compiler:  gcc temperature.c -o temperature
  *
  *         Author:  Hugo Valle (), hugovalle1@weber.edu
  *   Organization:  WSU
@@ -19,20 +19,35 @@
 // Main Function
 int main()
 {
-    float temp = 7.9;   // Celsius
-    double f_temp = 0;
-    // For floats use the %f placeholder
-    // for doubles use the %lf placeholder
-    printf("The temperature is %f Celsius\n", temp);
-    // Now take user input
-    printf("What is the temperature in Ogden now in Celsius: ");
-    // use %f to capture float information
-    // use %lf to capture double information
-    scanf("%f", &temp);
-    // COnvert from Celsius to Fahrenheit
-    f_temp = (temp * 9/5) + 32;
-    printf("I see, your temp is %lf in Fahrenheit.\n",f_temp);
-
+    double in_temp, out_temp;
+    int choice;
+    // 1) Ask user which temperature mode they want to use
+    printf("Welcome to the temperature calculator\n");
+    printf("Plese enter your choice:\n");
+    printf("\t0 for Celsius or 1 for Fahrenheit: ");
+    scanf("%d", &choice);
+    // 2) Based on choice calculate Ceisius or Fahrenheit
+    if(choice == 0)
+    {
+        printf("Enter the temperature in Celsius: ");
+        scanf("%lf", &in_temp);
+        // Convert from Celsius to Fahrenheit
+        out_temp = (in_temp * 9.0/5.0) + 32;
+        printf("Your temperature in Fahrenheit is: %.2lf\n", out_temp);
+    }
+    else if (choice == 1)
+    {
+        printf("Enter the temperature in Fahrenheit: ");
+        scanf("%lf", &in_temp);
+        // Convert from Fahrenheit to Celsius
+        out_temp = (in_temp - 32) * 5.0/9.0;
+        printf("Your temperature in Celsius is: %.2lf\n", out_temp);
+    }
+    else
+    {
+        printf("Sorry, you did not entered 0 or 1\nAdios amigo\n");
+        return 0;
+    }
     return 0;
 }
 // Function Definitions
