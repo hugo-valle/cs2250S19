fd1a90bcf48fcc2e29f7aea2c5739ba9f4303245
Hugo Valle <hugovalle1@weber.edu>
2019-01-31 09:19:03 -0700

    Initial commit

---
+++ mod4/month_day.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  month_day.c
+ *
+ *    Description:  Take month and day from user, then verify entries are valid.
+ *
+ *        Version:  1.0
+ *        Created:  01/31/2019 09:16:58 AM
+ *       Revision:  none
+ *       Compiler:  gcc month_day.c -o month_day.out 
+ *
+ *         Author:  Hugo Valle (), hugovalle1@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Main Function
+int main()
+{
+    // 1) Capture user input
+    
+    // 2) Use IF to validate month
+
+    // 3) Use SWITCH to validate day of the month
+
+    return 0;
+}
