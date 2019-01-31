ff47d58a791d9748d6f016ccb73a3e47c89a3c46
Hugo Valle <hugovalle1@weber.edu>
2019-01-31 08:38:19 -0700

    initial commit

---
+++ mod4/angle.c
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  angle.c
+ *
+ *    Description:  Calculate the quadrant based on input angle
+ *                  Quadrant I: angle between 0 and 90
+ *                  Quadrant II:angle between 90 and 180
+ *                  Quadrant III:angle between 180 and 270
+ *                  Quadrant IV:angle between 270 and 360
+ *
+ *        Version:  1.0
+ *        Created:  01/31/2019 08:35:03 AM
+ *       Revision:  none
+ *       Compiler:  gcc angle.c -o angle.out 
+ *
+ *         Author:  Hugo Valle (), hugovalle1@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
