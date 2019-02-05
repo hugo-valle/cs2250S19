8709bc1d739a9801fc26c27a8231fbf641b4c274
Hugo Valle <hugovalle1@weber.edu>
2019-02-05 08:51:52 -0700

    Calculate average

---
+++ mod5/firstwhile.c
@@ -0,0 +1,43 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  firstwhile.c
+ *
+ *    Description:  learn while loops
+ *
+ *        Version:  1.0
+ *        Created:  02/05/2019 08:36:46 AM
+ *       Revision:  none
+ *       Compiler:  gcc firstwhile.c -o firstwhile.out
+ *
+ *         Author:  Hugo Valle (), hugovalle1@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+// Constants
+#define MAXCOUNT 4
+// Main Function
+int main()
+{
+    // Task 1: Calculate the average of user input values
+    int count = 0;
+    float grade, avg;
+    float total = 0;
+
+    while(count < MAXCOUNT)
+    {
+        printf("\nEnter %d hw grade(0-100): ", count + 1);
+        scanf("%f", &grade);
+        total += grade;     // add up grades
+        count++;            // update test condition: sentinel
+    }
+    avg = total/MAXCOUNT;
+    printf("Your avg is [%6.2f]\n", avg);
+    printf("\nBye amigo\n");
+    return 0;
+}
+// Function Definitions
+
+
