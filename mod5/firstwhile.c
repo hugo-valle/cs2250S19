5c3806307f5621dd4fdf168c586d6c4f8c1ec61d
Hugo Valle <hugovalle1@weber.edu>
2019-02-05 09:15:51 -0700

    Task 3: Add request for number of entries

--- mod5/firstwhile.c
+++ mod5/firstwhile.c
@@ -17,15 +17,23 @@
  */
 #include <stdio.h>
 // Constants
-#define MAXCOUNT 4
+#define MINCOUNT 2
+#define MAXCOUNT 5
 // Main Function
 int main()
 {
     // Task 1: Calculate the average of user input values
     // Task 2: Validate user input for 0 to 100 only
+    // Task 3: Ask user how many values they need to enter
     int count = 0;
     float grade, avg;
     float total = 0;
+    int student_count;
+
+    printf("How many hw values you want to enter (between %d and %d? ", 
+            MINCOUNT, MAXCOUNT);
+    scanf("%d", &student_count);
+
 
     while(count < MAXCOUNT)
     {
@@ -38,6 +46,10 @@ int main()
         }
         total += grade;     // add up grades
         count++;            // update test condition: sentinel
+        if(count == student_count)
+        {
+            break;          // reach the student_count
+        }
     }
     avg = total/MAXCOUNT;
     printf("Your avg is [%6.2f]\n", avg);
