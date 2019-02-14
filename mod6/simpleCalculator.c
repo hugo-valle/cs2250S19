d850500be7c738c934e486c81453ed3f1889846a
Hugo Valle <hugovalle1@weber.edu>
2019-02-14 10:18:49 -0700

    Add it to loop

--- mod6/simpleCalculator.c
+++ mod6/simpleCalculator.c
@@ -17,11 +17,17 @@
  * =====================================================================================
  */
 #include <stdio.h>
+#include <stdlib.h>  // atoi() string to integer
 // Constants
-
+#define EXIT 0
+#define ADD 1
+#define SUB 2
+#define MUL 3
 // Main Function
 int main(int argc, char* argv[])
 {
+    int choice = 0;
+    int num1, num2;
     // Validate argument count, 
     // return 1 otherwise
     if(argc != 3)
@@ -30,6 +36,33 @@ int main(int argc, char* argv[])
         printf("Usage: %s <NUM1> <NUM2>\n", argv[0]);
         return 1;
     }
+    // Convert input strings to integers
+    num1 = atoi(argv[1]); // first input param
+    num2 = atoi(argv[2]); // second input param
+
+    do      // beg of do while
+    {
+        printf("What do you want to do with these numbers?\n");
+        printf("\t 1) Addition\n");
+        printf("\t 2) Subtraction\n");
+        printf("\t 3) Multiplication\n");
+        printf("\t 0) To quit\n");
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            case ADD:
+                printf("The %d + %d is %d\n", num1, num2, num1 + num2);
+                break;
+            case SUB:
+                printf("The %d - %d is %d\n", num1, num2, num1 - num2);
+                break;
+            case MUL:
+                printf("The %d * %d is %d\n", num1, num2, num1 * num2);
+                break;
+            default:
+                printf("Unsupported function\n");
+        }
+    } while(choice != EXIT); // end of do while
 
     printf("Adios amigo\n");
     return 0;
