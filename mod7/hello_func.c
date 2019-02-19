2f19626981a240b15c71fef13cd69500d4cd149d
Hugo Valle <hugovalle1@weber.edu>
2019-02-19 10:14:01 -0700

    Add BMI function

--- mod7/hello_func.c
+++ mod7/hello_func.c
@@ -24,11 +24,15 @@ void Hello_World();
 void Hello_Number(int num);
 int Get_Square(int num);
 int Get_Number();
+int Get_Sum(int num1, int num2);
+double Get_BMI(double weight, double height);
 
 // Main Function
 int main(int argc, char* argv[])
 {
     int i1 = 23;
+    int num1, num2, sum;
+
     Hello_World();
 
     Hello_Number(i1);
@@ -38,6 +42,17 @@ int main(int argc, char* argv[])
 
     i1 = Get_Number();
     Hello_Number(i1);
+
+    num1 = Get_Number();
+    Hello_Number(num1);
+    num2 = Get_Number();
+    Hello_Number(num2);
+    sum = Get_Sum(num1, num2);
+    Hello_Number(sum);
+
+    double bmi = Get_BMI(65.8, 1.89);
+    printf("your  BMI is [%lf]\n", bmi);
+    printf("your  BMI is [%lf]\n", Get_BMI(65.8, 1.89));
 
     return 0;
 }
@@ -108,4 +123,40 @@ int Get_Number()
     printf("Please enter an int value: ");
     scanf("%d", &input);
     return input;
+}
+
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Get_Sum
+ *  Description:  Return the sum of two integers
+ *  Param1: First integer
+ *  Param2: Second integer
+ *  Return: First + Second
+ * =====================================================================================
+ */
+int Get_Sum(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Get_BMI
+ *  Description:  Calculate the Body Max Index
+ *                  BMI = weight(kg)/height(m)^2
+ *      Param1: Weight in kg
+ *      Param2: Height in meters
+ *      Return: BMI
+ * =====================================================================================
+ */
+double Get_BMI(double weight, double height)
+{
+    double bmi;
+    // ALgorithm
+    bmi = weight/(height * height);
+    return bmi;
 }
