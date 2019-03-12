aaeed5a9ac1226cebfbc76cd6c9c2ba0499e9e2b
Hugo Valle <hugovalle1@weber.edu>
2019-03-12 09:41:00 -0600

    Add gross and net pay

--- mod9/employee.c
+++ mod9/employee.c
@@ -25,19 +25,34 @@ typedef struct Employee
     int idNum;
     double payRate;
     double hours;
+    double netPay;
+    double taxRate;
+    double grossPay;
+
 } Employee;
+
+#define TAXRATE1 0.10    // 10%
+#define TAXRATE2 0.25    // 25%
+#define TAXRATE3 0.75    // 75%
 
 // Function Prototypes
 double CalcNet(Employee emp);  // pass by value
+double CalcGross(Employee* emp);  // pass by reference
 
 // Main Function
 int main(int argc, char* argv[])
 {
-    double netPay = 0;
     Employee waldo = {1, 7.50, 20};
-    netPay = CalcNet(waldo);
+    // Set tax rate
+    waldo.taxRate = TAXRATE1;
+    
+    waldo.netPay = CalcNet(waldo);          // pass by value
     printf("Hi Waldo, your net pay for %lf hours at %lf rate is %lf\n",
-            waldo.hours, waldo.payRate, netPay);
+            waldo.hours, waldo.payRate, waldo.netPay);
+
+    waldo.grossPay = CalcGross(&waldo);     // pass by reference
+    printf("Hi Waldo, your gross pay for %lf hours at %lf rate is %lf\n",
+            waldo.hours, waldo.payRate, waldo.grossPay);
 
     return 0;
 }
@@ -53,5 +68,21 @@ int main(int argc, char* argv[])
  */
 double CalcNet(Employee emp)
 {
-    return emp.payRate * emp.hours;
+    return emp.payRate * emp.hours * (1 - emp.taxRate);
+}
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  CalcGross
+ *  Description:  Calculate the gross pay based on payRate and hours
+ *  Param: Employee structure
+ *  Return: Gross Pay as double
+ * =====================================================================================
+ */
+double CalcGross(Employee* emp) // pass by reference
+{
+    // To access structure members when using a pointer (address)
+    // use the dereference operator  "->"
+    return emp->payRate * emp->hours;
 }
