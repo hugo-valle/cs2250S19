a77f8f1ae7ebdc1f25db66b2d9d21e5a22a1675d
Hugo Valle <hugovalle1@weber.edu>
2019-04-04 10:16:33 -0600

    Add logic to populate a vector and display the results

--- mod12/first_real_class.cpp
+++ mod12/first_real_class.cpp
@@ -18,72 +18,54 @@
  */
 // For C++ Code
 #include <iostream>
+#include <iomanip>  // more cout options
+#include <string>  // for strings
+#include <vector>  // for vectors "arrays with lots of features"
+#include "Movie.h"
 using namespace std;
 // Constants and Globals
-class Movie
-{
-    // Only the member has access to
-    // data members are private
-    private:
-        string title = "";
-        int year = 0;
-   // Anyone has access to it
-   // Member functions are public
-    public:
-        // "Setters"
-        void set_title(string set_title);
-        void set_year(int set_year);
-        // "Getters"
-        string get_title();
-        int get_year();
-
-}; // end of Movie class
 
 // Main Function
 int main(int argc, char* argv[])
 {
-    // Create an "object"
-    Movie movie;  // create an "instance" of this class Movie
-    // set your value
-//    movie.title = "Batman";    // illegal - no direct access
-    movie.set_title("Batman");
-    movie.set_year(2009);
-//    movie.set_year(1887);
+    cout << "The movie list Program" << endl << endl
+        << "Enter a movie ..." << endl << endl;
+    // Get movies from user
+//    Movie movies[10];  // an array of 10 movies. Fixed to 10 spots
+    vector<Movie> movies;  // define a vector of movies. Grow on demand
+    char another = 'y';
+    // Loop for entries
+    while(tolower(another) == 'y')
+    {
+        Movie movie;
+        string title;
+        int year;
+        // Get user input
+        cout << "Title: ";   // get title
+        getline(cin, title);
+        movie.set_title(title);
+        cout << "Year: ";   // get year
+        cin >> year;
+        movie.set_year(year);
+        // Add it to the vector
+        movies.push_back(movie);
 
-    cout << "MOVIE DATA" << endl
-        << "Title " << movie.get_title() << endl
-        << "Year " << movie.get_year() << endl;
-
-
-
+        cout << endl << "Enter another movie? (y|n): ";
+        cin >> another;
+        cin.ignore();  // ignore anything else in the buffer
+        cout << endl;
+    }// end of while loop
+    // Display movies
+    const int w = 10;
+    cout << left
+        << setw(w*3) << "TITLE"
+        << setw(w) << "YEAR" << endl;
+    for(Movie movie : movies)  // looping over vector
+    {
+        cout << setw(w*3) << movie.get_title()
+            << setw(w) << movie.get_year() << endl;
+    }
+    cout << endl;
 
     return 0;
 }
-// Function Defenitions
-void Movie::set_title(string set_title)
-{
-    title = set_title;
-    return;
-}
-
-void Movie::set_year(int set_year)
-{
-    if (set_year < 1888)
-    {
-        throw invalid_argument("Year must be 1888 or later.");
-    }
-    year = set_year;
-    return;
-}
-
-// Define Getters
-string Movie::get_title()
-{
-    return title;
-}
-        
-int Movie::get_year()
-{
-    return year;
-}
-
