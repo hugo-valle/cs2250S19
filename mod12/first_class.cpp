fbe869f30dbb43e03a1c29c740ff602817297a8b
Hugo Valle <hugovalle1@weber.edu>
2019-04-04 08:54:37 -0600

    Added member function to Movie structure

--- mod12/first_class.cpp
+++ mod12/first_class.cpp
@@ -24,9 +24,11 @@ struct Movie
 {
     string title = "";          // data member
     int year = 0;               // data member
+    bool equals(const Movie& to_compare);  // member function declaration
 };
 // Function Prototypes
 bool Equals(const Movie& my_movie, const Movie& to_compare);  // the address operator is at the end of the name
+
 // Main Function
 int main(int argc, char* argv[])
 {
@@ -40,30 +42,49 @@ int main(int argc, char* argv[])
     
     Movie movie2;  // create a Movie object
     movie2.title = "Batman";  // set title
-    movie2.year = 2009;      // set year
+    movie2.year = 2008;      // set year
 
     cout << "MOVIE DATA" << endl
         << "Title:   " << movie2.title << endl
         << "Year:    " << movie2.year << endl;
 
     // Test for equal values
+    // The equivalent of typing: movie == movi2
     if(Equals(movie, movie2))
     {
-        cout << "Movies are equal" << endl;
+        cout << "PF: Movies are equal" << endl;
     }
     else
     {
-        cout << "Movies are not equal" << endl;
+        cout << "PF: Movies are not equal" << endl;
+    }
+    // Now use the member function instead of the program function
+    // movie is comparing to movie2
+    if(movie.equals(movie2))
+    {
+        cout << "MF: Movies are equal" << endl;
+    }
+    else
+    {
+        cout << "MF: Movies are not equal" << endl;
     }
 
 
     return 0;
 }
-// Function Defenitions
+// Function Definitions
 bool Equals(const Movie& my_movie, const Movie& to_compare)
 {
     return (my_movie.title == to_compare.title && 
             my_movie.year == to_compare.year);
 }
+
+// Member function definition
+bool Movie::equals(const Movie& to_compare)
+{
+    return(title == to_compare.title &&
+            year == to_compare.year);
+}
+
 
 
