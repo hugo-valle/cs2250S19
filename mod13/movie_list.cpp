520ea60ebf612f2bd4b8043012c5f0e10bda9532
Hugo Valle <hugovalle1@weber.edu>
2019-04-09 09:43:43 -0600

    Initial commit

---
+++ mod13/movie_list.cpp
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  movie_list.cpp
+ *
+ *    Description:  Create a movie list program. Loads information from txt file
+ *                  then, it presents the user with a menu to interact with the data.
+ *
+ *        Version:  1.0
+ *        Created:  04/09/2019 09:41:40 AM
+ *       Revision:  none
+ *       Compiler (C++):  g++ movie_list.cpp Movie.cpp -o movie_list.out
+ *          Usage:  ./movie_list.out 
+ *
+ *         Author:  Hugo Valle (), hugovalle1@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+// For C++ Code
+#include <iostream>
+#include "Movie.h"
+using namespace std;
+// Constants and Globals
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
+// Function Defenitions
+
+
